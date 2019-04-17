24ff607499893e00e4e982ee3cbd2bfca1358846
Marco <43689503+ValeroM@users.noreply.github.com>
2019-04-16 23:37:31 -0400

    Get data from PandaScore API (#10)

--- server/controllers/home/overwatch.js
+++ server/controllers/home/overwatch.js
@@ -1,7 +1,27 @@
+const request = require('superagent');
+
+//Put this token in a separate file and git ignore it but for now WEEEEE
+const token = 'K-vNqWhG9uYGlPgD-SunEJu9bLZNfw_uOBi_2rGPgIp7b56V5lM';
+
 const setup = () => {
+  
   const printReq = (req, res, next) => {
     console.log("I HIT THE HOME/Overwatch BACKEND");
     next();
+  };
+
+  const accessPanda = (req, res, next) => {
+    request
+    .get(`api.pandascore.co/ow/matches?token=${token}`)
+    .set('Accept', 'application/json')
+    .then(res => {
+      for (let arr of res.body)
+        console.log(`Tournament name: ${arr['serie'].full_name}\nFrom league: ${arr['league'].name}\nStarts at: ${arr['tournament'].begin_at}\n`)
+      next();
+    })
+    .catch(function(error) {
+      console.log(`Failed to list league matches\n${error.message}`);
+    });
   };
 
   const sendResponse = (req, res, next) => {
@@ -12,6 +32,7 @@ const setup = () => {
   
   return [
     printReq,
+    accessPanda,
     sendResponse
   ];
 };
