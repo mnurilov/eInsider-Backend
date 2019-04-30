2709a7f6637120794611286f5d52d11d6abb2aaa
mnurilov <michaelnurilov67@gmail.com>
2019-04-29 21:30:33 -0400

    Updated home endpoint to return the most upcoming tournament for each of the 4 games in a json

--- server/controllers/home/upcomingTournaments.js
+++ server/controllers/home/upcomingTournaments.js
@@ -16,89 +16,97 @@ const setup = () => {
     request
     .get(`https://api.pandascore.co/lol/tournaments/upcoming?sort=begin_at&token=${token}`)
     .set('Accept', 'application/json')
-    .then(res => { // stores data in an array about the next upcoming tournament for lol as an object. 
-      upcomingTournaments = res.body[0];
-      console.log(JSON.stringify(upcomingTournaments, null, 2));
+    .then(res => {
+      upcomingTournaments["lol"] = {
+        Game: "League of Legends",
+        Tournament_Name: res.body[0].name,
+        Start_Time: res.body[0].begin_at,
+        End_Time: res.body[0].end_at,
+        League_Name: res.body[0].league.name,
+        Serie_Name: res.body[0].serie.name
+      };
       next();
     })
-    .catch(function(error) {
-      console.log(`Failed get lol upcoming tournament data ${error.message}`);
+    .catch(err => {
+      console.log(`Failed get lol upcoming tournament data ${err.message}`);
     });
   };
 
-  /*const accessPanda = (req, res, next) => {
+  const GetNextUpcomingTournamentDota2 = (req, res, next) => {
     request
-    .get(`api.pandascore.co/ow/tournaments/upcoming?token=${token}`)
+    .get(`https://api.pandascore.co/dota2/tournaments/upcoming?sort=begin_at&token=${token}`)
     .set('Accept', 'application/json')
     .then(res => {
-      for (let arr of res.body)  // appending info to array that we're sending to front end
-        objectData.push({ "Tournament_name": arr['serie'].full_name, "Detailed_name": arr['name'], "League": arr['league'].name, "Starts_at": arr['begin_at'] })
+      upcomingTournaments["dota2"] = {
+        Game: "Dota 2",
+        Tournament_Name: res.body[0].name,
+        Start_Time: res.body[0].begin_at,
+        End_Time: res.body[0].end_at,
+        League_Name: res.body[0].league.name,
+        Serie_Name: res.body[0].serie.name
+      };
       next();
     })
-    .catch(function(error) {
-      console.log(`Failed to get OW upcoming tournaments\n${error.message}`);
+    .catch(err => {
+      console.log(`Failed get dota2 upcoming tournament data ${err.message}`);
     });
-  };*/
+  };
+
+  const GetNextUpcomingTournamentCSGO = (req, res, next) => {
+    request
+    .get(`https://api.pandascore.co/csgo/tournaments/upcoming?sort=begin_at&token=${token}`)
+    .set('Accept', 'application/json')
+    .then(res => {
+      upcomingTournaments["csgo"] = {
+        Game: "Counter-Strike: Global Offensive",
+        Tournament_Name: res.body[0].name,
+        Start_Time: res.body[0].begin_at,
+        End_Time: res.body[0].end_at,
+        League_Name: res.body[0].league.name,
+        Serie_Name: res.body[0].serie.name
+      };
+      next();
+    })
+    .catch(err => {
+      console.log(`Failed get csgo upcoming tournament data ${err.message}`);
+    });
+  };
+
+  const GetNextUpcomingTournamentOW = (req, res, next) => {
+    request
+    .get(`https://api.pandascore.co/ow/tournaments/upcoming?sort=begin_at&token=${token}`)
+    .set('Accept', 'application/json')
+    .then(res => {
+      upcomingTournaments["ow"] = {
+        Game: "Overwatch",
+        Tournament_Name: res.body[0].name,
+        Start_Time: res.body[0].begin_at,
+        End_Time: res.body[0].end_at,
+        League_Name: res.body[0].league.name,
+        Serie_Name: res.body[0].serie.name
+      };
+      next();
+    })
+    .catch(err => {
+      console.log(`Failed get ow upcoming tournament data ${err.message}`);
+    });
+  };
 
   const sendResponse = (req, res, next) => {
+    console.log("Sending back the following json:\n" + JSON.stringify(upcomingTournaments, null, 2));
     res
-    .status(200);
-    //.json(objectData);
+    .status(200)
+    .json(upcomingTournaments);
   };
   
   return [
     logEndpoint,
     GetNextUpcomingTournamentLol,
+    GetNextUpcomingTournamentDota2,
+    GetNextUpcomingTournamentCSGO,
+    GetNextUpcomingTournamentOW, 
     sendResponse
   ];
 };
 
 module.exports = setup;
-
-/*const GetNextUpcomingTournamentCSGO = (req, res, next) => {
-  request
-  .get(`api.pandascore.co/csgo/tournaments/upcoming?token=${token}`)
-  .set('Accept', 'application/json')
-  .then(res => { // stores data in an array about the next upcoming tournament for csgo as an object. 
-    objectData.push({ "Game": "CS:GO", "Tournament_name": res.body[0]['serie'].full_name, "Detailed_name": res.body[0]['name'], "League": res.body[0]['league'].name, "Starts_at": res.body[0]['begin_at'] })
-    next();
-  })
-  .catch(function(error) {
-    console.log(`Failed get csgo data under /routes/home\n${error.message}`);
-  });
-};
-
-const GetNextUpcomingTournamentDota2 = (req, res, next) => {
-  request
-  .get(`api.pandascore.co/dota2/tournaments/upcoming?token=${token}`)
-  .set('Accept', 'application/json')
-  .then(res => { // stores data in an array about the next upcoming tournament for dota2 as an object. 
-    objectData.push({ "Game": "Dota2", "Tournament_name": res.body[0]['serie'].full_name, "Detailed_name": res.body[0]['name'], "League": res.body[0]['league'].name, "Starts_at": res.body[0]['begin_at'] })
-    next();
-  })
-  .catch(function(error) {
-    console.log(`Failed get dota2 data under /routes/home\n${error.message}`);
-  });
-};
-
-
-
-const GetNextUpcomingTournamentOW = (req, res, next) => {
-  request
-  .get(`api.pandascore.co/ow/tournaments/upcoming?token=${token}`)
-  .set('Accept', 'application/json')
-  .then(res => { // stores data in an array about the next upcoming tournament for ow as an object.
-    objectData.push({ "Game": "Overwatch", "Tournament_name": res.body[0]['serie'].full_name, "Detailed_name": res.body[0]['name'], "League": res.body[0]['league'].name, "Starts_at": res.body[0]['begin_at'] })
-    next();
-  })
-  .catch(function(error) {
-    console.log(`Failed get OW data under /routes/home\n${error.message}`);
-  });
-};
-
-return [
-  GetNextUpcomingTournamentCSGO,
-  GetNextUpcomingTournamentDota2,
-  GetNextUpcomingTournamentLol,
-  GetNextUpcomingTournamentOW, 
-];*/
