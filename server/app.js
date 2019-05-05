823b180df2d36518e9452be3f9ea3d515cd57649
Michael Nurilov <33560995+mnurilov@users.noreply.github.com>
2019-05-05 16:37:33 -0400

    Fixed delete method issue (#22)

--- server/app.js
+++ server/app.js
@@ -29,6 +29,10 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
   next();
 });
 
